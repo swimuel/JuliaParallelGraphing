765f2933354888b137bfff452159535695352b60
William Ning <12452williamning@gmail.com>
2019-04-23 10:40:55 +1200

    Prims is kinda parallel now, but seems to be slower

--- src/ParallelGraphing.jl
+++ src/ParallelGraphing.jl
@@ -8,15 +8,36 @@ using LightGraphs
 using SimpleWeightedGraphs
 using Distributed
 
-export prims_parallel
+export prims_sequential, prims_parallel
+
+
+#=helper function to create a connected SimpleWeightedGraph
+	=#
+function make_Simple_Weighted_Graph(size)
+	g = SimpleWeightedGraph(size)
+	for i in 2:size
+		add_edge!(g,i-1,i,1)
+		#add some junk edges which algorthim has to filter out_matrix
+		source = rand(1:i)
+		destination = rand(1:i)
+		if !has_edge(g,source,destination)
+			add_edge!(g,source,destination,10)
+		end 
+
+	end
+	return g 
+end
 
 #=helper function that takes a SimpleWeightedGraph and outputs an
-	adjacency matrix
+	adjacency matrix. Made parallel with a parallel for loop. 
+	inputs: @graph: SimpleWeightedGraph type input graph
+	outputs: @out_matrix: Adjacency matrix
 =#
 function graph_to_matrix(graph)
 	output_matrix = Matrix(undef,nv(graph),nv(graph))
-		
-	for source in vertices(graph)
+
+	#Made parallel.
+	Threads.@threads for source in vertices(graph)
 		for destination in vertices(graph)
 			if has_edge(graph,source,destination)
 				output_matrix[source,destination] = graph.weights[source,destination]
@@ -28,6 +49,8 @@ function graph_to_matrix(graph)
 	return output_matrix			
 end
 
+
+#helper function to construct a graph once the parents have been found for prims
 function construct_graph(parent,matrix)
 	source = Int64[]
 	destination = Int64[]
@@ -37,25 +60,24 @@ function construct_graph(parent,matrix)
 		push!(destination,i)
 		push!(weights,matrix[i,parent[i]])
 	end
-	println(source)
-	println(destination)
-	println(weights)
 	return SimpleWeightedGraph(source,destination,weights)
 end
 
-function prims_parallel(graph)
+
+#=Sequential Variant of the prims algorithm to find the minimum spanning tree
+=#
+function prims_sequential(graph)
 	if !is_connected(graph)
 		throw("Graph not connected")
 	end
-	
+
 	matrix = graph_to_matrix(graph)
 	#from the matrix, we shall do prims 
 	
-	parent = []
-	key = []
-	mstSet = []
+	parent = [] #Sources array
+	key = [] #Weights array
+	mstSet = [] #Whether or not element is in the minimum spanning tree
 	
-
 	#initialize the array
 	for i in 1:nv(graph)
 		push!(key,Inf)
@@ -70,6 +92,7 @@ function prims_parallel(graph)
 		#pick minimum key vertex from set of vertices not in mst
 		min = Inf
 		min_index = -1		
+		
 		for v in 1:nv(graph)
 			if mstSet[v] ==false && key[v] < min
 				min = key[v]
@@ -77,20 +100,71 @@ function prims_parallel(graph)
 			end
 		end
 		#add picked vertex to the MST
-		mstSet[min_index] = true
+		mstSet[min_index] = true	
 
+		#update the sources and keys array
 		for i in 1:nv(graph)
 			if matrix[min_index,i]!=0 && mstSet[i] == false && matrix[min_index,i] < key[i]
 				parent[i] = min_index
 				key[i] = matrix[min_index,i]
-			end
+			end			
 		end
-		
 	end 
 	graph = construct_graph(parent,matrix)
 	return graph
 end
 
+
+#parallelized Variant of the prims algorithms, uses the Threads.@threads construct
+function prims_parallel(graph)
+	if !is_connected(graph)
+		throw("Graph not connected")
+	end
+
+	matrix = graph_to_matrix(graph)
+	#from the matrix, we shall do prims 
+	
+	parent = [] #Sources array
+	key = [] #Weights array
+	mstSet = [] #Whether or not element is in the minimum spanning tree
+	
+	#initialize the array
+	for i in 1:nv(graph)
+		push!(key,Inf)
+		push!(mstSet,false)
+		push!(parent,0)
+	end
+
+	key[1] = 0
+	parent[1] = -1
+
+	for vertex in 1:nv(graph)
+		#pick minimum key vertex from set of vertices not in mst
+		min = Inf
+		min_index = -1
+
+		#TODO make parallel with a critical path instead of reduction 
+		Threads.@threads for v in 1:nv(graph)
+			if mstSet[v] ==false && key[v] < min
+				min = key[v] #Fairly sure this has to made thread safe.
+				min_index= v 
+			end
+		end
+		
+		#add picked vertex to the MST
+		mstSet[min_index] = true		
+
+		#update the sources and keys array. made parallel
+		Threads.@threads for i in 1:nv(graph)
+			if matrix[min_index,i]!=0 && mstSet[i] == false && matrix[min_index,i] < key[i]
+				parent[i] = min_index
+				key[i] = matrix[min_index,i]
+			end			
+		end		
+	end 
+	graph = construct_graph(parent,matrix)
+	return graph
+end
 
 end # module
 
